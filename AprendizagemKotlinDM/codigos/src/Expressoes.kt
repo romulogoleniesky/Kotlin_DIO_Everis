ff7e9f0e50e569ca1a51785aa1ed2b6d9b53720d
RomuloGoleniesky <goleniesky@hotmail.com>
2021-04-01 12:37:38 -0300

    Três formas de fazer uma expressção, no corpo de bloco, corpo de expressão e omitindo a declaração do tipo de variavel.

--- AprendizagemKotlinDM/codigos/src/Expressoes.kt
+++ AprendizagemKotlinDM/codigos/src/Expressoes.kt
@@ -1,1 +1,16 @@
 //01/04/2021- Expressões.
+fun main() {
+    println(max(a = 10,b = 5))
+    println(max1(a = 10,b = 5))
+    println(max2(a = 10,b = 5))
+}
+
+// Corpo do Bloco:
+fun max(a:Int, b:Int):Int {
+    return if (a > b) a else b
+}
+// Corpo de expressão:
+fun max1(a:Int, b: Int):Int = if (a>b) a else b
+
+//Também podemos omitir a declaração do tipo Inteiro:
+fun max2(a:Int, b: Int) = if (a>b) a else b
