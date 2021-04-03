e7421fdcd30fb8c588c1543605d106598e8441c9
RomuloGoleniesky <goleniesky@hotmail.com>
2021-04-03 20:09:22 -0300

    Criado as definições de Val e Var.

---
+++ AprendizagemKotlinDM/codigos/src/VAL_VAR.kt
@@ -0,0 +1,14 @@
+// Val e Var
+// Em Kotlin declaramos as variáveis de duas formas:
+// val (valor) : Referência imutável.
+// var (variável) : Referência mutável.
+
+val nome = "Nome"
+var idade= 20
+var x = idade +30
+
+fun main(){
+     println(nome)
+     println(idade)
+     println(x)
+ }
