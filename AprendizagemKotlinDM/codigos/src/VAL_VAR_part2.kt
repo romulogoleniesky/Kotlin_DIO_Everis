a1d79aa1c358fbd346d75ad53faae710fa386ebf
RomuloGoleniesky <goleniesky@hotmail.com>
2021-04-03 21:51:35 -0300

    Criado as definições de Val e Var_ part 2

---
+++ AprendizagemKotlinDM/codigos/src/VAL_VAR_part2.kt
@@ -0,0 +1,18 @@
+fun main(){
+    //Val imutável:
+    val nome : String
+    nome = "Rômulo"
+    //Na tentativa de alterar o nome, não funcionou:
+    //nome = "Sophia"
+    println(nome)
+
+    var idade : Int
+    idade = 30
+    println(idade)
+
+    //Alterando o Var idade:
+
+    idade = 32
+    println(idade)
+
+}
